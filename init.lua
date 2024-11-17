ee8b0700d6fbb8563edb05744ee30c7971378a90
Aaditya Murthy <aaditya@murthy.me>
2024-11-16 19:10:49 -0600

    update init.lua

--- init.lua
+++ init.lua
@@ -279,7 +279,6 @@ require('lazy').setup({
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
       require('which-key').setup {
         icons = {
           -- set icon mappings to true if you have a Nerd Font
@@ -319,55 +318,7 @@ require('lazy').setup({
         },
       }
 
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-        ['<leader>g'] = {
-          name = 'Debug',
-          s = {
-            name = 'Step',
-            c = { "<cmd>lua require('dap').continue()<CR>", 'Continue' },
-            v = { "<cmd>lua require('dap').step_over()<CR>", 'Step Over' },
-            i = { "<cmd>lua require('dap').step_into()<CR>", 'Step Into' },
-            o = { "<cmd>lua require('dap').step_out()<CR>", 'Step Out' },
-          },
-          h = {
-            name = 'Hover',
-            h = { "<cmd>lua require('dap.ui.variables').hover()<CR>", 'Hover' },
-            v = { "<cmd>lua require('dap.ui.variables').visual_hover()<CR>", 'Visual Hover' },
-          },
-          u = {
-            name = 'UI',
-            h = { "<cmd>lua require('dap.ui.widgets').hover()<CR>", 'Hover' },
-            f = { "local widgets=require('dap.ui.widgets');widgets.centered_float(widgets.scopes)<CR>", 'Float' },
-          },
-          r = {
-            name = 'Repl',
-            o = { "<cmd>lua require('dap').repl.open()<CR>", 'Open' },
-            l = { "<cmd>lua require('dap').repl.run_last()<CR>", 'Run Last' },
-          },
-          b = {
-            name = 'Breakpoints',
-            c = {
-              "<cmd>lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
-              'Breakpoint Condition',
-            },
-            m = {
-              "<cmd>lua require('dap').set_breakpoint({ nil, nil, vim.fn.input('Log point message: ') })<CR>",
-              'Log Point Message',
-            },
-            t = { "<cmd>lua require('dap').toggle_breakpoint()<CR>", 'Create' },
-          },
-          c = { "<cmd>lua require('dap').scopes()<CR>", 'Scopes' },
-          i = { "<cmd>lua require('dap').toggle()<CR>", 'Toggle' },
-        },
-      require('which-key').add {
+            require('which-key').add {
         { '<leader>c', group = '[C]ode' },
         { '<leader>c_', hidden = true },
         { '<leader>d', group = '[D]ocument' },
